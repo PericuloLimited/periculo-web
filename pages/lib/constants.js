0f12e49ded15c33eb228e1a5582fa6e1a5854e56
Tom Godwin <tom.godwin@periculo.co.uk>
2021-04-20 14:14:10 +0100

    reset

--- pages/lib/constants.js
+++
@@ -1,6 +0,0 @@
-export const CMS_URL = 'https://cms.usewarden.com'
-export const SOCKET_IO_ENDPOINT = "https://api.usewarden.com/";
-//export const HOST = "http://localhost:3000/";
-export const HOST = "https://wardenmaster.vercel.app/";
-
-
